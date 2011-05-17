ed7d6a937800a1551f3d3646a5012a8cc979adcd
John Bintz <john@coswellproductions.com>
2011-05-17 07:47:42 -0400

    switch to rake task and gh-pages hosting the readme

--- lib/jasmine-headless-webkit.rb
+++ lib/jasmine-headless-webkit.rb
@@ -1,7 +1,7 @@
 module Jasmine
   module Headless
     module Webkit
-      # Your code goes here...
     end
   end
 end
+
---
+++ lib/jasmine/headless/task.rb
@@ -0,0 +1,20 @@
+module Jasmine
+  module Headless
+    class Task
+      attr_accessor :colors, :keep_on_error, :jasmine_config
+
+      def initialize(name = 'jasmine:headless')
+        @colors = false
+        @keep_on_error = false
+        @jasmine_config = nil
+
+        yield self if block_given?
+
+        desc 'Run Jasmine specs headlessly'
+        task name do
+          system %{jasmine-headless-webkit #{@colors ? "-c" : "--no-colors"} #{@keep_on_error ? "--keep" : ""} #{@jasmine_config ? "-j #{@jasmine_config}" : ""}}
+        end
+      end
+    end
+  end
+end
