cc619466dffaa9f31d78a7aca6ba2917b412df7a
John Bintz <john@coswellproductions.com>
2011-06-12 10:40:18 -0400

    punch sprockets in the duck to make it compile files we can actually test

--- lib/jasmine/headless/railtie.rb
+++ lib/jasmine/headless/railtie.rb
@@ -1,11 +1,40 @@
 require 'jasmine/headless/task'
 
+module Digest
+  class JasmineTest
+    def self.file(file)
+      new
+    end
+
+    def file(file)
+      self
+    end
+    
+    def hexdigest
+      'test'
+    end
+
+    def update(prefix)
+      self
+    end
+  end
+end
+
 module Jasmine
   module Headless
     class Railtie < Rails::Railtie
       rake_tasks do
         Jasmine::Headless::Task.new do |t|
           t.colors = true
+        end
+
+        if Rails.version >= "3.1.0"
+          desc 'Force generate static assets without an MD5 hash, all assets end with -test.<ext>'
+          task 'assets:precompile:for_testing' => :environment do
+            Rails.application.assets.instance_variable_set(:@digest_class, Digest::JasmineTest)
+
+            Rake::Task['assets:precompile'].invoke
+          end
         end
       end
     end
