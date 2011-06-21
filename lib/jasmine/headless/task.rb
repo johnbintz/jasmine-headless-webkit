00c4a30481cf0b4b519eda54232ec39db877e95f
John Bintz <john@coswellproductions.com>
2011-06-21 06:49:00 -0400

    fix railtie for rails 3.1, fixes #22

--- lib/jasmine/headless/task.rb
+++ lib/jasmine/headless/task.rb
@@ -1,5 +1,25 @@
 require 'jasmine/headless/runner'
 
+module Digest
+  class JasmineTest
+    def self.file(file)
+      new
+    end
+
+    def file(file)
+      self
+    end
+    
+    def hexdigest
+      'test'
+    end
+
+    def update(prefix)
+      self
+    end
+  end
+end
+
 module Jasmine
   module Headless
     class Task
@@ -21,6 +41,15 @@ module Jasmine
             :remove_html_file => !@keep_on_error, 
             :jasmine_config => @jasmine_config
           )
+        end
+
+        if Rails.version >= "3.1.0"
+          desc 'Force generate static assets without an MD5 hash, all assets end with -test.<ext>'
+          task 'assets:precompile:for_testing' => :environment do
+            Rails.application.assets.digest_class = Digest::JasmineTest
+
+            Rake::Task['assets:precompile'].invoke
+          end
         end
       end
     end
