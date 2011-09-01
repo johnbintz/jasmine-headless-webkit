bd8fa3e53685ce65a74638eef209a0818d770d55
John Bintz <john@coswellproductions.com>
2011-09-01 08:36:11 -0400

    appease rubygems

--- ext/jasmine-webkit-specrunner/extconf.rb
+++ ext/jasmine-webkit-specrunner/extconf.rb
@@ -7,3 +7,4 @@ require 'qt/qmake'
 system %{make clean}
 Qt::Qmake.make!('jasmine-headless-webkit', 'specrunner.pro')
 
+FileUtils.cp File.expand_path('../Makefile.dummy', __FILE__), File.expand_path('../Makefile', __FILE__)
