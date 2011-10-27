3a559fb0d5b0dfd08bd9d019000335125f1e352b
John Bintz <john@coswellproductions.com>
2011-10-26 22:20:51 -0400

    more cleaning

--- vendor/assets/coffeescripts/jasmine.headless-reporter.coffee
+++
@@ -1,5 +0,0 @@
-if !jasmine?
-  throw new Error("jasmine not laoded!")
-
-# The reporter itself.
-class jasmine.HeadlessReporter
--- vendor/assets/javascripts/jasmine.headless-reporter.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  if (!(typeof jasmine !== "undefined" && jasmine !== null)) {
-    throw new Error("jasmine not laoded!");
-  }
-  jasmine.HeadlessReporter = (function() {
-    function HeadlessReporter() {}
-    return HeadlessReporter;
-  })();
-}).call(this);
