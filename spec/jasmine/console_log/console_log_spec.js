a18100bb71e58aa91f79f26634899093d4632877
John Bintz <john@coswellproductions.com>
2011-11-21 14:13:15 -0500

    move the jquery console test to the right spot

--- spec/jasmine/console_log/console_log_spec.js
+++ spec/jasmine/console_log/console_log_spec.js
@@ -3,12 +3,5 @@ describe('console.log', function() {
     console.log("hello");
     expect(success).toEqual(1);
   });
-
-  it("wont eat my precious jqueries", function() {
-    var d = $('<div><div id="inner">b</div></div>');
-    expect(d.find('#inner').length).toBe(1);
-    console.log(d.find('#inner'));
-    expect(d.find('#inner').length).toBe(1);
-  });
 });
 
---
+++ spec/javascripts/console.log_spec.coffee
@@ -0,0 +1,13 @@
+describe 'console.log', ->
+  it 'should not eat my precious jqueries', ->
+    _log = JHW.log
+
+    JHW.log = ->
+
+    d = $('<div><div id="inner">b</div></div>');
+    expect(d.find('#inner').length).toBe(1);
+    console.log(d.find('#inner'));
+    expect(d.find('#inner').length).toBe(1);
+
+    JHW.log = _log
+
